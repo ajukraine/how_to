085232a453c31d6dc81cb38f0bc500c847f27bc0
Bohdan Makohin <ajukraine@gmail.com>
2022-08-28 14:11:01 +0300

    feat(vim_shell): install custom (for exampple, old) version of Brew package

---
+++ via_shell/brew_install_custom_version.sh
@@ -0,0 +1,4 @@
+brew tap-new ajukraine/homebrew-versions
+brew extract --version 0.14.11 hashicorp/tap/terraform ajukraine/versions
+brew install ajukraine/homebrew-versions/terraform@0.14.10
+brew install ajukraine/homebrew-versions/vim@8.2.5150
